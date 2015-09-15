42e8c2ab6088ca74df9b62b442abcbdde42d340e
tgdwindt <bdamanmaster8@hotmail.com>
2015-09-15 12:20:05 +0200

    structuur verandert

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div id="container">
+    <div id="header">
+        <?php include "libs/header.php"; ?>
+    </div>
+    <div id="content">
+
+    </div>
+    <div id="footer">
+
+    </div>
+
+
+</div>
+
+</body>
+</html>
